9453b6a4184b83f2713ae3c95693faeee176a233
clitustsai <infoclituspc@gmail.com>
2026-04-16 12:57:02 +0700

    fix: xoa FeedbackButton khoi app layout, khong che noi dung

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,11 +1,9 @@
 import AppLayoutClient from "../../components/AppLayoutClient";
-import FeedbackButton from "../../components/FeedbackButton";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <AppLayoutClient>
       {children}
-      <FeedbackButton />
     </AppLayoutClient>
   );
 }
